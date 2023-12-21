b95458e12af3ea0e73a9196936cce2c57fa625e0
Nicolias <marat.khakimov.04@gmail.com>
2023-12-22 03:12:28 +0500

    Complete

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alarm : MonoBehaviour
+{
+    [SerializeField] private AudioSource _alarm;
+
+    [SerializeField] private float _alarmSpeed;
+
+    private float _maxVolume = 1;
+    private float _minVolume = 0;
+
+    private bool _isTriggered;
+
+    private void Update()
+    {
+        if (_isTriggered)
+            _alarm.volume = Mathf.MoveTowards(_alarm.volume, _maxVolume, _alarmSpeed * Time.deltaTime);
+        else
+            _alarm.volume = Mathf.MoveTowards(_alarm.volume, _minVolume, _alarmSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out FrauderMovement frauder))
+            _isTriggered = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out FrauderMovement frauder))
+            _isTriggered = false;
+    }
+}
--- Assets/Scripts/FrauderMovement.cs
+++ Assets/Scripts/FrauderMovement.cs
@@ -1,18 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class FrauderMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _moveSpeed;
+
+    private Collision2D _wall;
+
+    private Rigidbody2D _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _rigidbody.velocity = (new Vector2(
+            Input.GetAxis("Horizontal") * Time.deltaTime,
+            Input.GetAxis("Vertical") * Time.deltaTime).normalized) * _moveSpeed;
+
+        if (_wall != null)
+        {
+            float projection = Vector2.Dot(_rigidbody.velocity, _wall.contacts[0].normal);
+
+            if (projection < 0)
+                _rigidbody.velocity -= projection * _wall.contacts[0].normal;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        _wall = collision;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        _wall = null;   
     }
 }
